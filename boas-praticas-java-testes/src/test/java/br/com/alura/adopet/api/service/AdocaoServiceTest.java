2830cb55db6557e859b839bed9fbb8a3d6cd5e17
GabryelBoeira <GabryelBoeira@users.noreply.github.com>
2024-09-04 20:04:47 -0300

    Utilizando @Spy

--- boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ boas-praticas-java-testes/src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -1,8 +1,8 @@
 package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
-import br.com.alura.adopet.api.exception.ValidacaoException;
 import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
@@ -12,15 +12,17 @@ import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -41,8 +43,14 @@ class AdocaoServiceTest {
     @Mock
     private EmailService emailService;
 
+    @Spy
+    private List<ValidacaoSolicitacaoAdocao> validacoes = new ArrayList<>();
+
     @Mock
-    private List<ValidacaoSolicitacaoAdocao> validacoes;
+    private ValidacaoSolicitacaoAdocao validador1;
+
+    @Mock
+    private ValidacaoSolicitacaoAdocao validador2;
 
     @Mock
     private Abrigo abrigo;
@@ -53,34 +61,56 @@ class AdocaoServiceTest {
     @Mock
     private Pet pet;
 
-    @Mock
-    private SolicitacaoAdocaoDto dto;
-
     @InjectMocks
     private AdocaoService service;
+
+    @Captor
+    private ArgumentCaptor<Adocao> captorCaptor;
+
+    private SolicitacaoAdocaoDto dto;
+
 
     @Test
     @DisplayName("Cenário para solicitar adocao com sucesso")
     void cenarioSolicitarAdocao01() {
-        given(abrigo.getEmail()).willReturn("email@br.com");
+        given(abrigo.getEmail()).willReturn("email@gmail.com");
         given(pet.getAbrigo()).willReturn(abrigo);
-        when(petRepository.getReferenceById(any())).thenReturn(pet);
+        when(petRepository.getReferenceById(pet.getId())).thenReturn(pet);
+        when(tutorRepository.getReferenceById(tutor.getId())).thenReturn(tutor);
+        this.dto = new SolicitacaoAdocaoDto(pet.getId(), tutor.getId(),"Novo mascote");
 
         service.solicitar(dto);
 
-        then(adocaoRepository).should().save(any());
+        //Verifica se a adocao foi salva/criada
+        then(adocaoRepository).should().save(captorCaptor.capture());
+        Adocao adocao = captorCaptor.getValue();
+        assertEquals(pet, adocao.getPet());
+        assertEquals(tutor, adocao.getTutor());
+        assertEquals(dto.motivo(), adocao.getMotivo());
+
+        //Verifica se o email foi enviado
         then(emailService).should().enviarEmail(anyString(), anyString(), anyString());
     }
 
     @Test
-    @DisplayName("Cenário para solicitar adocao com falha no processo de validação")
+    @DisplayName("Cenário para solicitar adocao chama os validadores no processo de solicitacao")
     void cenarioSolicitarAdocao02() {
-        assertThrows(ValidacaoException.class, () -> service.solicitar(dto));
+        this.dto = new SolicitacaoAdocaoDto(pet.getId(), tutor.getId(),"Novo mascote");
+        given(abrigo.getEmail()).willReturn("email@gmail.com");
+        given(pet.getAbrigo()).willReturn(abrigo);
+        when(petRepository.getReferenceById(pet.getId())).thenReturn(pet);
+        when(tutorRepository.getReferenceById(tutor.getId())).thenReturn(tutor);
+        validacoes.add(validador1);
+        validacoes.add(validador2);
 
+        service.solicitar(dto);
+
+        then(validador1).should().validar(dto);
+        then(validador2).should().validar(dto);
     }
 
     @Test
-    @DisplayName("")
+    @DisplayName("sdfsdfsdf")
     void cenarioSolicitarAdocao03() {
 
     }
